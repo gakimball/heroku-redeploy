0ae4d0b6cf5055734568ebbea55c29a3ae668d69
Geoff Kimball <geoff@geoffkimball.com>
2019-05-30 12:57:59 -0700

    Fix failing tests

--- test/index.js
+++ test/index.js
@@ -1,18 +1,18 @@
 const {expect} = require('chai');
-const handler = require('../lib/handler');
+const deployer = require('../lib/deployer');
 
 const args = ['one=app-one&two=app-two', '12345'];
 
 describe('Express handler', () => {
   it('throws if env query string is not set', () => {
-    expect(() => handler()).to.throw(Error);
+    expect(() => deployer()).to.throw(Error);
   });
 
   it('throws if API key is not set', () => {
-    expect(() => handler(args[0])).to.throw(Error);
+    expect(() => deployer(args[0])).to.throw(Error);
   });
 
   it('returns a function', () => {
-    expect(handler(...args)).to.be.a('function');
+    expect(deployer(...args)).to.be.a('function');
   });
 });
